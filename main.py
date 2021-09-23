ba7c4e67173bf0dda7210e3a34bde1f0f718110c
David Pittman <dpittmancontact@gmail.com>
2021-09-23 07:01:21 -0400

    Create main.py for testing purposes

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+myName = input("Enter your name: ")
+if myName == "David":
+    print_hi("me")
+elif myName == "Spiderman":
+    print_hi("Peter")
+else:
+    print_hi("stranger")

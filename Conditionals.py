8023fdad111122526ad1ddc5b785a4810ddab394
David Pittman <dpittmancontact@gmail.com>
2021-09-24 06:12:06 -0400

    Add example explaining difference between 'is' and '=='

--- Conditionals.py
+++ Conditionals.py
@@ -1,6 +1,7 @@
 
 # Examples of how to utilize conditionals in Python
 
+# if and elif example w/ 'or' conditional
 def ifs_in_python(num):
     if num > 10:
         print('Your number is greater than 10')
@@ -10,6 +11,18 @@ def ifs_in_python(num):
         print('Your number is either 1 or 0')
     else:
         print('Your number is a negative number')
+
+# 'is' vs '=='
+# 'is' basically checks if the variables exists in the same place in memory
+# where '==' checks if the variables have the same values
+example = 1
+example == 1 # True
+example is 1 # True
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+a == b # True
+a is b # False
 
 
 myNumber = input("Enter your favorite number: ")

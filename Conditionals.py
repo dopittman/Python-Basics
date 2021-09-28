35d39f7458c411739c0845d40e1dd28914562a56
David Pittman <dpittmancontact@gmail.com>
2021-09-28 06:34:11 -0400

    Prepare Conditionals.py for Rock, Paper, Scissors example

--- Conditionals.py
+++ Conditionals.py
@@ -1,7 +1,9 @@
-
 # Examples of how to utilize conditionals in Python
 
 # if and elif example w/ 'or' conditional
+import random
+
+
 def ifs_in_python(num):
     if num > 10:
         print('Your number is greater than 10')
@@ -12,12 +14,13 @@ def ifs_in_python(num):
     else:
         print('Your number is a negative number')
 
+
 # 'is' vs '=='
 # 'is' basically checks if the variables exists in the same place in memory
 # where '==' checks if the variables have the same values
-example = 1
-example == 1  # True
-example is 1  # True
+# example = 1
+# example == 1  # True
+# example is 1  # True
 
 a = [1, 2, 3]
 b = [1, 2, 3]
@@ -26,17 +29,20 @@ a is b  # False
 c = b
 b is c  # True
 
+# #  Bouncer Code snippet
+# age = input("How old are you?")
+#
+# if age:  # checks if age is truthy, ie. not an empty string
+#     age = int(age)
+#     if age >= 21:
+#         print("You are good to enter and can drink.")
+#     elif age >= 18:
+#         print("You can enter, but need a wristband.")
+#     else:
+#         print("You are too young and cannot enter.")
+# else:
+#     print("Please enter an age!")
 
-#  Bouncer Code snippet
-age = input("How old are you?")
-
-if age:  # checks if age is truthy, ie. not an empty string
-    age = int(age)
-    if age >= 21:
-        print("You are good to enter and can drink.")
-    elif age >= 18:
-        print("You can enter, but need a wristband.")
-    else:
-        print("You are too young and cannot enter.")
-else:
-    print("Please enter an age!")
+# Rock Paper Scissors Example
+player_choice = input("Rock, paper, or scissors?")
+computer_choice = random.randrange(0, 2)  # 0 = Rock, 1 = Paper, 2 = Scissors

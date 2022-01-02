71b947aa8ac0df2ab7450ee51c383638c6c1f838
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-01-02 15:39:05 -0500

    Add optimized Binary Search algo with pointers

--- SearchAlgos/BinarySearch.py
+++ SearchAlgos/BinarySearch.py
@@ -1,3 +1,5 @@
+
+# Unoptimized Binary search that copies the array each time
 # define binary_search()
 def binary_search(sorted_list, target):
   #   if list is empty, return 'value not found'
@@ -33,3 +35,35 @@ def binary_search(sorted_list, target):
 # For testing:
 sorted_values = [13, 14, 15, 16, 17]
 print(binary_search(sorted_values, 16))
+
+
+
+# Optimized Binary Search with Pointers
+
+def binary_search(sorted_list, left_pointer, right_pointer, target):
+  # this condition indicates we've reached an empty "sub-list"
+  if left_pointer >= right_pointer:
+    return "value not found"
+
+  # We calculate the middle index from the pointers now
+  mid_idx = (left_pointer + right_pointer) // 2
+  mid_val = sorted_list[mid_idx]
+
+  if mid_val == target:
+    return mid_idx
+  if mid_val > target:
+    # we reduce the sub-list by passing in a new right_pointer
+    # the middle index now becomes the right pointer
+    return binary_search(sorted_list, left_pointer, mid_idx, target)
+  if mid_val < target:
+    # we reduce the sub-list by passing in a new left_pointer
+    # the middle index now becomes the left pointer
+    return binary_search(sorted_list, mid_idx + 1, right_pointer, target)
+
+
+values = [77, 80, 102, 123, 288, 300, 540]
+start_of_values = 0
+end_of_values = len(values)
+result = binary_search(values, start_of_values, end_of_values, 288)
+
+print("element {0} is located at index {1}".format(288, result))

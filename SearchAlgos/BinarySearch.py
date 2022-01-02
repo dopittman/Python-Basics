f6ed8d393b52a94bb28005892402a1aa1ce24ab5
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-01-02 16:18:28 -0500

    Add Iterable version of binary search

--- SearchAlgos/BinarySearch.py
+++ SearchAlgos/BinarySearch.py
@@ -67,3 +67,35 @@ end_of_values = len(values)
 result = binary_search(values, start_of_values, end_of_values, 288)
 
 print("element {0} is located at index {1}".format(288, result))
+
+
+
+# Iterable version of binary search
+
+def binary_search(sorted_list, target):
+  left_pointer = 0
+  right_pointer = len(sorted_list)
+
+  # fill in the condition for the while loop
+  while left_pointer < right_pointer:
+    # calculate the middle index using the two pointers
+    mid_idx = (left_pointer + right_pointer) // 2
+    mid_val = sorted_list[mid_idx]
+    if mid_val == target:
+      return mid_idx
+    if target < mid_val:
+      # set the right_pointer to the appropriate value
+      right_pointer = mid_idx
+    if target > mid_val:
+      # set the left_pointer to the appropriate value
+      left_pointer = mid_idx + 1
+
+  return "Value not in list"
+
+
+# test cases
+print(binary_search([5, 6, 7, 8, 9], 9))
+print(binary_search([5, 6, 7, 8, 9], 10))
+print(binary_search([5, 6, 7, 8, 9], 8))
+print(binary_search([5, 6, 7, 8, 9], 4))
+print(binary_search([5, 6, 7, 8, 9], 6))

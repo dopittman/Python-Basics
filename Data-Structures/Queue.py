7d92d465cc4142250bbba0bdc4fd2f92268ba06c
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-01-06 18:08:08 -0500

    Add additional example for using a queue structure

--- Data-Structures/Queue.py
+++ Data-Structures/Queue.py
@@ -14,12 +14,13 @@ class Node:
     def get_value(self):
         return self.value
 
+#  Basic Queue
 class Queue:
     def __init__(self):
         self.head = None
         self.tail = None
 
-
+#  Queue with necessary functions
 class Queue:
     def __init__(self, max_size=None):
         self.head = None
@@ -79,3 +80,40 @@ class Queue:
 q = Queue()
 q.enqueue("some guy with a mustache")
 q.dequeue()
+
+
+
+
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue(10)
+print("Adding orders to our deli line...\n------------")
+deli_line.enqueue("egg and cheese on a roll")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+deli_line.enqueue("toasted sesame bagel with butter and jelly")
+deli_line.enqueue("toasted roll with butter")
+deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
+deli_line.enqueue("two fried eggs with home fries and ketchup")
+deli_line.enqueue("egg and cheese on a roll with jalapeos")
+deli_line.enqueue("plain bagel with plain cream cheese")
+deli_line.enqueue("blueberry muffin toasted with butter")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+# ------------------------ #
+# Uncomment the line below:
+deli_line.enqueue("western omelet with home fries")
+# ------------------------ #
+print("------------\nOur first order will be " + deli_line.peek())
+print("------------\nNow serving...\n------------")
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+# ------------------------ #
+# Uncomment the line below:
+deli_line.dequeue()
+# ------------------------ #

142344529c74847b8d335a17e8f9d83acdc3ceb8
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-01-19 16:43:43 -0500

    Add two-pointer solution

--- Leetcode/Two Pointers/977 - Squares of a Sorted Array.py
+++ Leetcode/Two Pointers/977 - Squares of a Sorted Array.py
@@ -12,14 +12,28 @@
 # Input: nums = [-7,-3,2,3,11]
 # Output: [4,9,9,49,121]
 
-testNums = [-4,-1,0,3,10]
-
-def sortedSquares(nums):
-    unsortedArray = []
-    for i in nums:
-        squaredNum = i * i
-        unsortedArray.append(squaredNum)
-    print(unsortedArray)
+test = [-13, -5, -1, 3, 6, 15]
+test2 = [-4,-1,0,3,10]
+test3 = [-7,-3,2,3,11]
 
 
-sortedSquares(testNums)
+def run(my_list):
+    n = len(my_list)
+    result = [0] * n
+    left_pointer = 0
+    right_pointer = n - 1
+
+    for i in range(n - 1, -1, -1):
+        if abs(my_list[left_pointer]) < abs(my_list[right_pointer]):
+            square = my_list[right_pointer]
+            result[i] = square * square
+            right_pointer -= 1
+        else:
+            square = my_list[left_pointer]
+            result[i] = square * square
+            left_pointer += 1
+    return result
+
+
+print(run([1]))
+

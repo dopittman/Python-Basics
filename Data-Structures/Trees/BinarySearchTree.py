6be1db212f5a374a2224ab753f0a119ff0a6df48
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-01-27 14:45:53 -0500

    Update Binary Search Tree with all necessary functionality and an example

--- Data-Structures/Trees/BinarySearchTree.py
+++ Data-Structures/Trees/BinarySearchTree.py
@@ -1,3 +1,6 @@
+import random
+
+
 class BinarySearchTree:
     def __init__(self, value, depth=1):
         self.value = value
@@ -6,28 +9,42 @@ class BinarySearchTree:
         self.right = None
 
     def insert(self, value):
-        if value < self.value:
-            # If there is no left node, we set left to equal value, and increase depth one level
-            if self.left == None:
+        if (value < self.value):
+            if (self.left is None):
                 self.left = BinarySearchTree(value, self.depth + 1)
                 print(f'Tree node {value} added to the left of {self.value} at depth {self.depth + 1}')
             else:
-                # If left value exists for this node, then we move the next left node and call insert on it
                 self.left.insert(value)
         else:
-            if self.right == None:
-                # if there is no right node we set right to equal value, and increase depth one level
+            if (self.right is None):
                 self.right = BinarySearchTree(value, self.depth + 1)
                 print(f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
             else:
-                # If right value exists for this node then we move to the next right node then call insert again
                 self.right.insert(value)
 
+    def get_node_by_value(self, value):
+        if (self.value == value):
+            return self
+        elif ((self.left is not None) and (value < self.value)):
+            return self.left.get_node_by_value(value)
+        elif ((self.right is not None) and (value >= self.value)):
+            return self.right.get_node_by_value(value)
+        else:
+            return None
 
-# Insert values below:
+    def depth_first_traversal(self):
+        if (self.left is not None):
+            self.left.depth_first_traversal()
+        print(f'Depth={self.depth}, Value={self.value}')
+        if (self.right is not None):
+            self.right.depth_first_traversal()
 
-root = BinarySearchTree(100)
-root.insert(50)
-root.insert(125)
-root.insert(75)
-root.insert(25)
+
+print("Creating Binary Search Tree rooted at value 15:")
+tree = BinarySearchTree(15)
+
+for x in range(10):
+    tree.insert(random.randint(0, 100))
+
+print("Printing the inorder depth-first traversal:")
+tree.depth_first_traversal()
